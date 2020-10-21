8559e279137cca3304170d85de177410d45f716f
ankita1009 <66045040+ankita1009@users.noreply.github.com>
2020-10-21 06:53:27 -0700

    Update game.js

--- asynchronousBallMovement-master/asynchronousBallMovement-master/js/game.js
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/js/game.js
@@ -14,12 +14,71 @@ class Game{
         })
     }
 
-    start() {
+    async start() {
         if(gameState===0) {
             player=new Player();
-            player.getCount();
+            var playerCountreff=await database.ref('playerCount').once("value");
+            if(playerCountreff.exists()) {
+                playerCount=playerCountreff.val();
+                player.getCount();
+            }
+            
             form=new Form();
             form.display();
         }
+
+        car1=createSprite(100,200);
+        car1.addImage("car1",car1Image);
+        car2=createSprite(300,200);
+        car2.addImage("car2",car2Image);
+        car3=createSprite(500,200);
+        car3.addImage("car3",car3Image);
+        car4=createSprite(700,200);
+        car4.addImage("car4",car4Image);
+        cars=[car1,car2,car3,car4];
+        
+
     }
+
+    play() {
+        form.hide();
+        Player.getPlayerInfo();
+
+        if(allPlayers!==undefined) {
+            background(rgb(198,135,103));
+            image(trackImage,0,-displayHeight*4,displayWidth,displayHeight*5);
+           var index=0;
+           var x=175;
+           var y;
+            for(var i in allPlayers) {
+                index=index+1;
+                x=x+200;
+                y=displayHeight-allPlayers[i].distance
+                cars[index-1].x=x;
+                cars[index-1].y=y;
+                if(index===player.index) {
+                    cars[index-1].shapeColor="red"
+                    camera.position.x=displayWidth/2;
+                    camera.position.y=cars[index-1].y
+                }
+                
+            
+            }
+
+        }
+        if(keyIsDown(UP_ARROW) && player.index!==null) {
+            player.distance=player.distance+10;
+            player.update();
+        }
+
+        if(player.distance>3900) {
+            gameState=2
+        }
+        drawSprites();
+    }
+
+    end() {
+        console.log("game end");
+    }
+
 }
