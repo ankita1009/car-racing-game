57c97ee9f752a58855eacbca44d699d96469a469
ankita1009 <66045040+ankita1009@users.noreply.github.com>
2020-10-21 06:51:26 -0700

    Update form.js

--- asynchronousBallMovement-master/asynchronousBallMovement-master/js/form.js
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/js/form.js
@@ -1,25 +1,45 @@
 class Form{
     constructor() {
+        this.title=createElement('h2');
+        this.input = createInput("name");
+        this.button=createButton('play');
+        this.greeting=createElement('h3');
+        this.restart=createButton('Restart')
+    }
 
+    hide() {
+        this.button.hide();
+        this.input.hide();
+        this.greeting.hide();
+        this.title.hide();
     }
     display() {
-        var title=createElement('h2');
-        title.html('car racing game');
-        title.position(200,100);
-        var input = createInput("name");
-        input.position(200,250);
-        var button=createButton('play');
-        button.position(200,300);
-        var greeting=createElement('h3');
-        greeting.position(200,350);
-        button.mousePressed(function(){
-            input.hide();
-            button.hide();
-            var name=input.value();
+        this.title.html('car racing game');
+        this.title.position(displayWidth/2-50,0);
+        this.input.position(displayWidth/2-40,displayHeight/2-80);
+        this.button.position(displayWidth/2+30,displayHeight/2);
+        this.restart.position(displayWidth-100,20);
+       
+        
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+            player.name=this.input.value();
             playerCount+=1;
-            player.update(name);
+            player.index=playerCount;
+            player.update();
             player.updateCount(playerCount);
-            greeting.html('hey '+ name);
+            this.greeting.html('hey '+ player.name);
+            this.greeting.position(displayWidth/2-70,displayHeight/4);
         })
+
+        this.restart.mousePressed(()=>{
+            player.updateCount(0);
+            game.update(0);
+        })
+        
+
     }
+
+
 }
