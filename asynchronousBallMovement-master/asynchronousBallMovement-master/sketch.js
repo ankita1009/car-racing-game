fdd13bb13348fc641ad78c9b6f01e3d46cc97c59
ankita1009 <66045040+ankita1009@users.noreply.github.com>
2020-10-21 06:54:38 -0700

    Update sketch.js

--- asynchronousBallMovement-master/asynchronousBallMovement-master/sketch.js
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/sketch.js
@@ -1,11 +1,22 @@
 var database;
 var gameState=0,playerCount=0;
-var player;
+var player,allPlayers;
 var game,form;
+var cars,car1,car2,car3,car4;
+var car1Image,car2Image,car3Image,car4Image;
+var trackImage;
+
+function preload() {
+    car1Image=loadImage("Images/car1.png");
+    car2Image=loadImage("Images/car2.png");
+    car3Image=loadImage("Images/car3.png");
+    car4Image=loadImage("Images/car4.png");
+    trackImage=loadImage("Images/track.jpg");
+}
 
 function setup(){
     database=firebase.database();
-    createCanvas(500,500);
+    createCanvas(displayWidth-20,displayHeight-30);
     game=new Game();
     game.getState();
     game.start();
@@ -13,9 +24,18 @@ function setup(){
 }
 
 function draw(){
-    background("white");
-   
-    drawSprites();
+   if(playerCount===4){
+       game.update(1);
+   }
+
+   if(gameState===1) {
+       clear();
+       game.play();
+   }
+
+   if(gameState===2) {
+       game.end();
+   }
 }
 
 
