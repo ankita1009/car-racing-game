43b02c05e538a51e5eaed7ea6568023a56fdd3b5
ankita1009 <66045040+ankita1009@users.noreply.github.com>
2020-10-21 06:54:03 -0700

    Update player.js

--- asynchronousBallMovement-master/asynchronousBallMovement-master/js/player.js
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/js/player.js
@@ -1,7 +1,10 @@
 class Player{
     constructor() {
-
+        this.index=null;
+        this.name=null;
+        this.distance=0;
     }
+
     getCount() {
         var playerCountRef=database.ref('playerCount');
         playerCountRef.on("value",function(data) {
@@ -14,10 +17,18 @@ class Player{
         })
     }
 
-    update(name) {
-        var playerIndex='player' + playerCount;
+    update() {
+        var playerIndex='players/player' + this.index;
         database.ref(playerIndex).set({
-            name:name
+            name:this.name,
+            distance:this.distance
+        })
+    }
+
+    static getPlayerInfo() {
+        var playerInfo=database.ref('players');
+        playerInfo.on("value",(data)=>{
+            allPlayers=data.val();
         })
     }
 
